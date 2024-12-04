96b5e97428b424c6723fb8b8040149c922933a40
Daniel <161114752+danchousss@users.noreply.github.com>
2024-12-04 17:19:24 +0400

    init

---
+++ src/main/java/View/LoginView.java
@@ -0,0 +1,15 @@
+package View;
+
+import javax.swing.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class LoginView extends JFrame {
+    public LoginView(){
+        setTitle("Китеп Кана?");
+        setSize(800,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import View.LoginView;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginView loginView = new LoginView();
+    }
+}
